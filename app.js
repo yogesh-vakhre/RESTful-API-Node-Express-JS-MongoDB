7fbd54a10bfaf092d4724af55d8ff379fd2247e8
yogesh vakhre <yogesh.vakhre.5057@gmail.com>
2023-04-06 17:20:15 +0530

    delete old route files

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var router = require('./routes/index.routes'); 
-
 var app = express();
 
 // view engine setup
