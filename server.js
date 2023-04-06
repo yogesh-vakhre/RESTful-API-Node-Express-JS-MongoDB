31493b34b099f96d7d68e039221aaaf6405e86aa
yogesh vakhre <yogesh.vakhre.5057@gmail.com>
2023-04-06 16:14:43 +0530

    Configure the MongoDB Database

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL
+
+/*Setup mongoose database*/ 
+mongoose.connect(mongoString);
+const database = mongoose.connection
+
+database.on('error', (error) => {
+  console.log(error)
+})
+
+database.once('connected', () => {
+  console.log('Database Connected');
+})
+/*END Setup mongoose database*/
 
 const app = express();
 
