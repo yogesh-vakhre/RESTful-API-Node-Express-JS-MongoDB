913d66be1e72bb58155230ea0b2f225d2a32638b
yogesh vakhre <yogesh.vakhre.5057@gmail.com>
2023-04-06 18:33:49 +0530

    Add user crude

---
+++ models/user.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, default: null },    
+    email: { type: String, unique: true },
+    password: { type: String },
+    published: Boolean,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("user", userSchema);
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const usersController = require("../controllers/user.controller.js");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+ // Create a new User
+ router.post("/", usersController.create);
+
+ // Retrieve all users
+ router.get("/", usersController.findAll);
+
+ // Retrieve all published users
+ router.get("/published", usersController.findAllPublished);
+
+ // Retrieve a single User with id
+ router.get("/:id", usersController.findOne);
+
+ // Update a User with id
+ router.put("/:id", usersController.update);
+
+ // Delete a User with id
+ router.delete("/:id", usersController.delete);
+
+ // Create a new User
+ router.delete("/", usersController.deleteAll);
 
 module.exports = router;
