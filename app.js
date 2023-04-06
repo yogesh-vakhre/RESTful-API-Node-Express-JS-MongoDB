0bac6850987954531db3f15abbc56d4c152d85e5
yogesh vakhre <yogesh.vakhre.5057@gmail.com>
2023-04-06 17:01:17 +0530

    Added display default route  for application

--- app.js
+++ app.js
@@ -19,8 +19,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+ 
 app.use('/users', usersRouter);
+
+// Default page    
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Welcome to RESTful API"
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
