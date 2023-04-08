fd5cffbe67421d30e367782438022e1964f8c61e
yogesh vakhre <yogesh.vakhre.5057@gmail.com>
2023-04-08 14:33:36 +0530

    Install HPP Package and express middleware to protect against HTTP Parameter Pollution attacks

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hpp = require('hpp');
 
 var indexRouter = require('./routes/index.routes');
  
@@ -12,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(hpp());
 //Setup all routes
 app.use('/api/v1/', indexRouter); 
 
