f5f3b64c5391a288d2580495183371b1c4c757bf
yogesh vakhre <yogesh.vakhre.5057@gmail.com>
2023-04-08 10:05:36 +0530

    Remove views folder and update app.js

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var router = require('./routes/index.routes'); 
-var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var indexRouter = require('./routes/index.routes');
+ 
+var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,7 +14,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Setup all routes
-app.use('/api/v1/', router); 
+app.use('/api/v1/', indexRouter); 
 
 // Default page    
 app.get("/", (req, res) => {
@@ -25,22 +22,6 @@ app.get("/", (req, res) => {
     status: "success",
     message: "Welcome to RESTful API"
   });
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
