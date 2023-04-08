db2029d8ab2fd5558764a0722dd44fb978fca836
yogesh vakhre <yogesh.vakhre.5057@gmail.com>
2023-04-08 17:33:00 +0530

    Add auth middleware for Authentication

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,11 +1,14 @@
 var express = require("express");
 var app = express();
+
+var authMiddleware = require("../middleware/auth.middleware");
+
 var authRouter = require("./auth.routes");
 var usersRouter = require("./users.routes");
 var categoriesRouter = require("./categories.routes");
 
 app.use("/auth", authRouter);
 app.use("/users", usersRouter);
-app.use("/categories", categoriesRouter);
+app.use("/categories", authMiddleware, categoriesRouter);
 
 module.exports = app;
